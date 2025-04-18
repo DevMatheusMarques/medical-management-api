d7f899e1d184a4623cc8e35dc78fa929f50fad3c
DevMatheusMarques <estudosfatecmhcm@gmail.com>
2025-04-17 23:02:54 -0300

    refactor: Settings Adjustment.

--- src/main/java/com/devmatheusmarques/medicalManagement/MedicalManagementApplication.java
+++ src/main/java/com/devmatheusmarques/medicalManagement/MedicalManagementApplication.java
@@ -2,8 +2,11 @@ package com.devmatheusmarques.medicalManagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(scanBasePackages = "com.devmatheusmarques.medicalManagement")
+@EnableFeignClients(basePackages = "com.devmatheusmarques.medicalManagement.service")
 public class MedicalManagementApplication {
 
 	public static void main(String[] args) {
